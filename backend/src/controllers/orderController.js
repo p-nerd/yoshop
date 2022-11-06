57b6f19f8cbb1387f4e397e72848a476bb78d641
Shihab Mahamud <shihab4t@gmail.com>
2022-11-06 12:34:11 +0600

    Get orders and Get order by id routes

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -29,5 +29,29 @@ export const createNewOrder = wrap(async (req, res, next) => {
 
     const createdOrder = await order.save();
 
-    res.status(201).json(createdOrder);
+    return res.status(201).json(createdOrder);
+});
+
+export const getOrders = wrap(async (req, res, next) => {
+    const order = await Order.find().populate("user", "name email");
+
+    if (!order) {
+        res.status(404);
+        throw new Error("Orders not found");
+    }
+
+    return res.status(200).json(order);
+});
+
+export const getOrderById = wrap(async (req, res, next) => {
+    const orderId = req.params.id;
+
+    const order = await Order.findById(orderId).populate("user", "name email");
+
+    if (!order) {
+        res.status(404);
+        throw new Error("Order not found");
+    }
+
+    return res.status(200).json(order);
 });
--- backend/src/routers/orderRouter.js
+++ backend/src/routers/orderRouter.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { createNewOrder } from "../controllers/orderController.js";
+import {
+    createNewOrder,
+    getOrderById,
+    getOrders,
+} from "../controllers/orderController.js";
 import protect from "../middlewares/protect.js";
+import { validId } from "../middlewares/validate.js";
 
 const orderRouter = Router();
 
@@ -10,5 +15,19 @@ const orderRouter = Router();
  * @access Private
  */
 orderRouter.post("/", [protect], createNewOrder);
+
+/**
+ * @desc Get all orders
+ * @route GET /api/orders
+ * @access Private
+ */
+orderRouter.get("/", [protect], getOrders);
+
+/**
+ * @desc Get order by ID
+ * @route GET /api/orders/:id
+ * @access Private
+ */
+orderRouter.get("/:id", [protect, validId], getOrderById);
 
 export default orderRouter;
